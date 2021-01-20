1a65bd016cd0d1e98866d6702b081ef6e5b9ce62
Richard Seward <raseward14@gmail.com>
2021-01-19 21:11:55 -0700

    fixed timer, quiz template displayed, welcome hidden

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,18 @@
 // define variables
 var startBtn = document.querySelector("#start");
 var timeEl = document.querySelector("#timer");
-var mainEl = document.getElementById("main");
+var welcomeEl = document.getElementById("welcome");
+var formEl = document.querySelector("#form")
+
 var secondsLeft = 60;
+
+var pEl = document.querySelector("#question");
+var labelA = document.getElementById("option-a");
+var labelB = document.getElementById("option-b");
+var labelC = document.getElementById("option-c");
+var labelD = document.getElementById("option-d");
+
+var myScore = 0;
 
 // start quiz function
 function startQuiz () {
@@ -10,25 +20,36 @@ function startQuiz () {
 
     // start timer
     setTime();
-    function setTime () {
+    function setTime() {
 
         // set time interval
         var timerInterval = setInterval(function() {
             secondsLeft--;
             timeEl.textContent = "Timer: " + secondsLeft;
-
+            
             // if timer hits zero, end quiz
             if (secondsLeft === 0) {
                 clearInterval(timerInterval);
-                sendMessage();
+                alert("Times up! Click OK to see your score.")
                 timerInterval.textContent = "Timer: " + secondsLeft;
             }
+            askQuestions();
         }, 1000); // every second run this code block
     }
 
-    sendMessage();
-    function sendMessage () {
-        alert("Times up! Click the start button to try again!")
+    function askQuestions() {
+        welcomeEl.setAttribute("style", "display: none;")
+        formEl.setAttribute("style", "display: block")
+        pEl.textContent = "Commonly used data types DO NOT include:";
+        labelA.textContent = "strings";
+        labelB.textContent = "booleans";
+        labelC.textContent = "alerts";
+        labelD.textContent = "numbers";
+
+        if (labelC.checked) {
+            myScore = myScore + 1;
+        }
+        
     }
 }
 
